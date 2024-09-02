0e7855ec0ae2a646167799173360ef028127b0e5
FimPablo <78551208+FimPablo@users.noreply.github.com>
2024-09-02 11:29:14 -0300

    Update nadaDeMais.py

--- nadaDeMais.py
+++ nadaDeMais.py
@@ -9,24 +9,22 @@ def lol():
         interval = random.uniform(30, 120)
         typeMove = random.randint(0, 1)
         
-        time.sleep(interval)
-        
         if typeMove:
             x = random.randint(0, screen_width - 1)
             y = random.randint(0, screen_height - 1)
         
             pyautogui.moveTo(x, y, duration=1.5)
             
-            continue
+        else:
+            aQuant = random.randint(2, 20)
         
-        aQuant = random.randint(2, 20)
-        
-        pyautogui.write('8', interval=0.01)
-        for _ in range(aQuant):
-            pyautogui.write('=', interval=0.01)
+            pyautogui.write('8', interval=0.01)
+            for _ in range(aQuant):
+                pyautogui.write('=', interval=0.01)
             
-        pyautogui.write('D', interval=0.01)
+            pyautogui.write('D', interval=0.01)
 
+        time.sleep(interval)
 
 if __name__ == "__main__":
     lol()
